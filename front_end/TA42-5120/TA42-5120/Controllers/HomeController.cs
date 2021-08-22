788ce70cdf829a99ed2938f4c1a4a247283cc9c1
Doreen0133 <57879304+Doreen0133@users.noreply.github.com>
2021-08-22 17:07:23 +0800

    1st version of front-end part
    1. Complete some layout in homepage(Without problem statement and button to another 3 parts)
    2. Deploy to Azure cloud platform

---
+++ front_end/TA42-5120/TA42-5120/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TA42_5120.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+            return View();
+        }
+
+        public ActionResult Data()
+        {
+            ViewBag.Title = "UV Data";
+            return View();
+        }
+
+        public ActionResult Educate()
+        {
+            ViewBag.Title = "Education";
+            return View();
+        }
+
+        public ActionResult Recommend()
+        {
+            ViewBag.Title = "Recommendation";
+            return View();
+        }
+    }
+}
